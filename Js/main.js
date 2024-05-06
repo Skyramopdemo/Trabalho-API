76295846bc0d586916673c1389b9031cef6ca615
Henrique G Albring <104649585+Skyramopdemo@users.noreply.github.com>
2024-05-06 18:24:49 +0000

    Tratamento de erros

--- Js/main.js
+++ Js/main.js
@@ -5,9 +5,9 @@ function searchAPI(div, id = 5){
 
     var options = ['marcas', 'modelos', 'anos', '']
 
+    var validador = true
     
     for(i = id; i <= 4; i++){
-        console.log(i)
         var item = document.querySelector('.div_'+i)
         item.innerHTML = '<option value="" selected disabled style="display: none;"> </option>'
         if(i == id+1){
@@ -15,51 +15,65 @@ function searchAPI(div, id = 5){
         }
     }
 
+
     for(i = 0; i < dados.length; i++){
-        if(dados[i].value != ""){
-            string += "/"+dados[i].value+"/"+options[i]
+        if(!dados[i].parentElement.hasAttribute('hidden')){
+            if(dados[i].value != ""){
+                string += "/"+dados[i].value+"/"+options[i]
+            }
+            else{
+                validador = false
+                alert("Algum campo não foi o campo "+ dados[i].getAttribute('name'))
+                break
+            }
         }
     }
-
-    const url = `https://parallelum.com.br/fipe/api/v1${string}`;
-    fetch(url, {method : "GET", }).then((resp) => resp.json()).then((data) => {showResults(data, div, id);})
+    if(validador){
+        const url = `https://parallelum.com.br/fipe/api/v1${string}`;
+        fetch(url, {method : "GET", }).then((resp) => resp.json()).then((data) => {showResults(data, div, id);})
+    }
 }
 
 function showResults(data, element){
-    var div = document.querySelector('.'+element+'_div')
-    var tipo = document.querySelector('.'+element+'_select');
+    if(data.error){
+        alert("Erro da pesquisa: "+data.error)
+    }else{
 
-
-    div.removeAttribute('hidden')
-
-    if(element == 'marca' || element == 'ano'){
-
-        console.log("1")
-        for(i = 0; i < data.length; i++){
-            console.log("1.1")
-            tipo.innerHTML += '<option value="'+data[i].codigo+'">'+data[i].nome+'</option>'
+        var div = document.querySelector('.'+element+'_div')
+        var tipo = document.querySelector('.'+element+'_select');
+    
+    
+        div.removeAttribute('hidden')
+    
+        if(element == 'marca' || element == 'ano'){
+    
+            for(i = 0; i < data.length; i++){
+                tipo.innerHTML += '<option value="'+data[i].codigo+'">'+data[i].nome+'</option>'
+            }
+        }
+    
+        if(element == 'modelo'){
+    
+            for(i = 0; i < data.modelos.length; i++){
+                tipo.innerHTML += '<option value="'+data.modelos[i].codigo+'">'+data.modelos[i].nome+'</option>'
+            }
+        }
+    
+        if(element == 'detalhes'){
+    
+            var valor = document.querySelector('.detalhes_valor')
+            var combustivel = document.querySelector('.detalhes_combustivel')
+            var fipe = document.querySelector('.detalhes_fipe')
+            var mes = document.querySelector('.detalhes_mes')
+    
+            valor.innerHTML = data.Valor
+            combustivel.innerHTML = data.Combustivel
+            combustivel.innerHTML += data.SiglaCombustivel
+            fipe.innerHTML = data.CodigoFipe
+            mes.innerHTML = data.MesReferencia
         }
     }
+    
 
-    if(element == 'modelo'){
-
-        for(i = 0; i < data.modelos.length; i++){
-            tipo.innerHTML += '<option value="'+data.modelos[i].codigo+'">'+data.modelos[i].nome+'</option>'
-        }
-    }
-
-    if(element == 'detalhes'){
-
-        var valor = document.querySelector('.detalhes_valor')
-        var combustivel = document.querySelector('.detalhes_combustivel')
-        var fipe = document.querySelector('.detalhes_fipe')
-        var mes = document.querySelector('.detalhes_mes')
-
-        valor.innerHTML = data.Valor
-        combustivel.innerHTML = data.Combustivel
-        combustivel.innerHTML += data.SiglaCombustivel
-        fipe.innerHTML = data.CodigoFipe
-        mes.innerHTML = data.MesReferencia
-    }
 
 }
